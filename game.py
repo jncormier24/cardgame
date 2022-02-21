e7a818a4cf8f8ad8454bafc327cc2c33ad1fdaf7
Joe Cormier <jcormier@mstech.local>
2022-02-21 09:29:49 -0500

    started game logic

---
+++ game.py
@@ -0,0 +1,17 @@
+import card
+import deck
+import player
+
+class Game:
+    """
+    lets play War!
+    We need:
+    player 1
+    player 2
+    the game loop
+    the war definition
+    the war logic
+    """
+    
+    def __init__(self):
+        pass
